e03e6a3ea720fe189820338c5886aea770fa8a77
johnswyoon <john.sw.yoon@gmail.com>
2020-12-09 04:36:31 -0500

    refactor

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import styled from "styled-components";
-import manWithPhone from "../assets/img/manWithPhone.svg";
-import bags from "../assets/img/bags.svg";
+// import manWithPhone from "../assets/img/manWithPhone.svg";
+// import bags from "../assets/img/bags.svg";
+import phoneFlat from "../assets/img/phoneFlat.svg";
 
 const Home = () => {
   return (
-    <Container>
-      <h1 className="text-center mt-4">Welcome to FoneSaver!</h1>
+    <HeroContainer>
+      <div className="mt-5 mr-5">
+        <div className="mr-5">
+          <h2>Build your audience</h2>
+          <h2>and grow your</h2>
+          <h2>brand on social</h2>
+          <h2>media</h2>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quam
+          nihil autem quidem unde quo perspiciatis illo rem, optio quia.
+        </p>
+      </div>
       <HeroImage>
-        <img src={manWithPhone} alt="Man holding a phone" />
-        <img id="bags" src={bags} alt="2 Shopping Bags" />
+        <div className="d-flex align-items-center">
+          <img src={phoneFlat} alt="Man holding a phone" />
+          {/* <img src={manWithPhone} alt="Man holding a phone" /> */}
+          {/* <img id="bags" src={bags} alt="2 Shopping Bags" /> */}
+        </div>
       </HeroImage>
-    </Container>
+    </HeroContainer>
   );
 };
+
+const HeroContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5rem 30rem;
+
+  img {
+    padding-left: 7rem;
+  }
+`;
 
 const HeroImage = styled.div`
   img {
--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -5,154 +5,156 @@ import jQuery, { $ } from "jquery";
 
 const Subscribe = () => {
   return (
-    <div id="mc_embed_signup">
-      <form
-        action="https://fone-saver.us7.list-manage.com/subscribe/post?u=65bc58081e87a900fe2f8301f&amp;id=5ca182c0f8"
-        method="post"
-        rel="noopener"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        target="_blank"
-        novalidate
-      >
-        <div id="mc_embed_signup_scroll">
-          <h2>Subscribe</h2>
-          <div className="indicates-required">
-            <span className="asterisk">*</span> indicates required
-          </div>
-          <div className="mc-field-group">
-            <label for="mce-EMAIL">
-              Email Address <span className="asterisk">*</span>
-            </label>
-            <input
-              type="email"
-              name="EMAIL"
-              className="required email"
-              id="mce-EMAIL"
-            />
-          </div>
-          <div className="mc-field-group">
-            <label for="mce-FNAME">
-              First Name <span className="asterisk">*</span>
-            </label>
-            <input
-              type="text"
-              name="FNAME"
-              className="required"
-              id="mce-FNAME"
-            />
-          </div>
-          <div className="mc-field-group">
-            <label for="mce-LNAME">
-              Last Name <span className="asterisk">*</span>
-            </label>
-            <input
-              type="text"
-              name="LNAME"
-              className="required"
-              id="mce-LNAME"
-            />
-          </div>
-          <div className="mc-address-group">
+    <div className="container">
+      <div id="mc_embed_signup" className="d-flex justify-content-center mt-5">
+        <form
+          action="https://fone-saver.us7.list-manage.com/subscribe/post?u=65bc58081e87a900fe2f8301f&amp;id=5ca182c0f8"
+          method="post"
+          rel="noopener"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          target="_blank"
+          novalidate
+        >
+          <div id="mc_embed_signup_scroll">
+            <h2>Subscribe</h2>
+            <div className="indicates-required">
+              <span className="asterisk">*</span> indicates required
+            </div>
             <div className="mc-field-group">
-              <label for="mce-ADDRESS-addr1">
-                Address <span className="asterisk">*</span>
+              <label for="mce-EMAIL">
+                Email Address <span className="asterisk">*</span>
+              </label>
+              <input
+                type="email"
+                name="EMAIL"
+                className="required email"
+                id="mce-EMAIL"
+              />
+            </div>
+            <div className="mc-field-group">
+              <label for="mce-FNAME">
+                First Name <span className="asterisk">*</span>
               </label>
               <input
                 type="text"
-                maxlength="70"
-                name="ADDRESS[addr1]"
-                id="mce-ADDRESS-addr1"
+                name="FNAME"
                 className="required"
+                id="mce-FNAME"
               />
             </div>
             <div className="mc-field-group">
-              <label for="mce-ADDRESS-addr2">Address Line 2</label>
+              <label for="mce-LNAME">
+                Last Name <span className="asterisk">*</span>
+              </label>
               <input
                 type="text"
-                maxlength="70"
-                name="ADDRESS[addr2]"
-                id="mce-ADDRESS-addr2"
+                name="LNAME"
+                className="required"
+                id="mce-LNAME"
               />
             </div>
+            <div className="mc-address-group">
+              <div className="mc-field-group">
+                <label for="mce-ADDRESS-addr1">
+                  Address <span className="asterisk">*</span>
+                </label>
+                <input
+                  type="text"
+                  maxlength="70"
+                  name="ADDRESS[addr1]"
+                  id="mce-ADDRESS-addr1"
+                  className="required"
+                />
+              </div>
+              <div className="mc-field-group">
+                <label for="mce-ADDRESS-addr2">Address Line 2</label>
+                <input
+                  type="text"
+                  maxlength="70"
+                  name="ADDRESS[addr2]"
+                  id="mce-ADDRESS-addr2"
+                />
+              </div>
+              <div className="mc-field-group size1of2">
+                <label for="mce-ADDRESS-city">City</label>
+                <input
+                  type="text"
+                  maxlength="40"
+                  name="ADDRESS[city]"
+                  id="mce-ADDRESS-city"
+                  className="required"
+                />
+              </div>
+              <div className="mc-field-group size1of2">
+                <label for="mce-ADDRESS-state">State/Province/Region</label>
+                <input
+                  type="text"
+                  maxlength="20"
+                  name="ADDRESS[state]"
+                  id="mce-ADDRESS-state"
+                  className="required"
+                />
+              </div>
+              <div className="mc-field-group size1of2">
+                <label for="mce-ADDRESS-zip">Postal / Zip Code</label>
+                <input
+                  type="text"
+                  maxlength="10"
+                  name="ADDRESS[zip]"
+                  id="mce-ADDRESS-zip"
+                  className="required"
+                />
+              </div>
+              <div className="mc-field-group size1of2">
+                <label for="mce-ADDRESS-country">Country</label>
+                <select
+                  name="ADDRESS[country]"
+                  id="mce-ADDRESS-country"
+                  className="required"
+                >
+                  <option value="164" selected>
+                    USA
+                  </option>
+                  <option value="30">Canada</option>
+                </select>
+              </div>
+            </div>
             <div className="mc-field-group size1of2">
-              <label for="mce-ADDRESS-city">City</label>
+              <label for="mce-PHONE">
+                Phone Number <span className="asterisk">*</span>
+              </label>
               <input
                 type="text"
-                maxlength="40"
-                name="ADDRESS[city]"
-                id="mce-ADDRESS-city"
+                name="PHONE"
                 className="required"
+                id="mce-PHONE"
               />
             </div>
-            <div className="mc-field-group size1of2">
-              <label for="mce-ADDRESS-state">State/Province/Region</label>
+            <div id="mce-responses" className="clear">
+              <div
+                className="response"
+                id="mce-error-response"
+                style={{ display: "none" }}
+              ></div>
+              <div
+                className="response"
+                id="mce-success-response"
+                style={{ display: "none" }}
+              ></div>
+            </div>
+            <div className="clear">
               <input
-                type="text"
-                maxlength="20"
-                name="ADDRESS[state]"
-                id="mce-ADDRESS-state"
-                className="required"
+                type="submit"
+                value="Subscribe"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+                className="button"
               />
             </div>
-            <div className="mc-field-group size1of2">
-              <label for="mce-ADDRESS-zip">Postal / Zip Code</label>
-              <input
-                type="text"
-                maxlength="10"
-                name="ADDRESS[zip]"
-                id="mce-ADDRESS-zip"
-                className="required"
-              />
-            </div>
-            <div className="mc-field-group size1of2">
-              <label for="mce-ADDRESS-country">Country</label>
-              <select
-                name="ADDRESS[country]"
-                id="mce-ADDRESS-country"
-                className="required"
-              >
-                <option value="164" selected>
-                  USA
-                </option>
-                <option value="30">Canada</option>
-              </select>
-            </div>
           </div>
-          <div className="mc-field-group size1of2">
-            <label for="mce-PHONE">
-              Phone Number <span className="asterisk">*</span>
-            </label>
-            <input
-              type="text"
-              name="PHONE"
-              className="required"
-              id="mce-PHONE"
-            />
-          </div>
-          <div id="mce-responses" className="clear">
-            <div
-              className="response"
-              id="mce-error-response"
-              style={{ display: "none" }}
-            ></div>
-            <div
-              className="response"
-              id="mce-success-response"
-              style={{ display: "none" }}
-            ></div>
-          </div>
-          <div className="clear">
-            <input
-              type="submit"
-              value="Subscribe"
-              name="subscribe"
-              id="mc-embedded-subscribe"
-              className="button"
-            />
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
