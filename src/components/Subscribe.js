e149cd5915ff71f886fe610afdd0b6a71f39edfa
johnswyoon <john.sw.yoon@gmail.com>
2020-12-09 02:41:26 -0500

    refactor

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,11 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Form, Button, Card, Alert, Col, Row } from "react-bootstrap";
 
 const Subscribe = () => {
   return (
-    <div>
-      <h1>Subscribe</h1>
+    <div
+      className="d-flex justify-content-center mt-5"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Form>
+          <Form.Group>
+            <Form.Row>
+              <Col>
+                <Form.Label>First Name</Form.Label>
+                <Form.Control placeholder="John"></Form.Control>
+              </Col>
+              <Col>
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control placeholder="Doe"></Form.Control>
+              </Col>
+            </Form.Row>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="johndoe@email.com"
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="johndoe@email.com"
+            ></Form.Control>
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 };
